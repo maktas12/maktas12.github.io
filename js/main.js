04946c681f327e044b81d0338bac778131c29457
maktas12 <302652321@student.rocmondriaan.nl>
2021-06-21 12:52:44 +0200

    het is nu responsive

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log("main.js loaded in");
+// berekent en schrijft hoeveel farenheit celcius is//
+function temperatureConverter(valNum) {
+    valNum = parseFloat(valNum);
+   document.querySelector(".outputCelcius").innerHTML=(valNum-32)/1.8;
+  }
+// berekent en schrijft hoeveel celcius farenheit is//
+  function temperatureConverter2(valNum2) {
+    valNum2 = parseFloat(valNum2);
+    document.querySelector(".outputFarenheit").innerHTML=(valNum2*1.8)+32;
+  } 
